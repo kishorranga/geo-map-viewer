edeb80d5fcfc166f95c1254d877e03defe11e140
kishorranga <kishor.ranga@gmail.com>
2023-09-04 21:36:19 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header(props) {
     return (<>
         <div className='header-container'>
             <span className="company-logo">
-                <span className="company-name-title">Fugro</span>
+                <span className="company-name-title">KishorRanga</span>
             </span>
             <span className='title'>Map Viewer</span>
         </div>
